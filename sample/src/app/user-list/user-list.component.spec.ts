72e621b1372b47a9ac511883cf755f3d4b46a50a
tcthanu <tcthanu@gmail.com>
2022-04-05 14:06:53 +0530

    updated the test Cases

--- sample/src/app/user-list/user-list.component.spec.ts
+++ sample/src/app/user-list/user-list.component.spec.ts
@@ -1,18 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable, of } from 'rxjs';
 import { AppModule } from '../app.module';
+import { User } from '../bean/user';
+import { UserService } from '../user.service';
 
 import { UserListComponent } from './user-list.component';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
+  let service: UserService;
   let fixture: ComponentFixture<UserListComponent>;
+  let userServiceSpy : jasmine.SpyObj<UserService>;
 
   beforeEach(async () => {
+   const spy=jasmine.createSpyObj('UserService',['getUserList']);
     await TestBed.configureTestingModule({
        declarations: [ UserListComponent ],
        imports: [
         AppModule
-      ]
+      ],
+      providers:[{provide:UserService ,useValue:spy}]
     })
     .compileComponents();
   });
@@ -20,10 +28,18 @@ describe('UserListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
+    userServiceSpy = TestBed.inject(UserService) as jasmine.SpyObj<UserService>;
+    const mockUserList=[{id:1,name:'Thanu',age:44}];
+     userServiceSpy.getUserList.and.returnValue(of(mockUserList));     
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('Ngoninit should call the service', () => {
+    component.ngOnInit();
+    expect(component.userList.length).toEqual(1);
+    expect(component.userList[0].name).toEqual('Thanu');
   });
 });
--- sample/src/app/user.service.spec.ts
+++ sample/src/app/user.service.spec.ts
@@ -1,21 +1,39 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+
 import { AppModule } from './app.module';
+import { User } from './bean/user';
 
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
-
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
   beforeEach(() => {
+    let spy= jasmine.createSpyObj('HttpClinet',['get']);
     TestBed.configureTestingModule({
       imports: [
-        AppModule
-      ]
+        AppModule,HttpClientModule
+      ],
+      providers:[{provide :HttpClient,useValue: spy}]
     });
-    service = TestBed.inject(UserService);
+    service = TestBed.inject(UserService);    
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('should be call the Http Client', () => { 
+    httpClientSpy=TestBed.inject(HttpClient) as jasmine.SpyObj<HttpClient>;   
+    const mockUserList=[{id:1,name:'Thanu',age:44}];
+    //spyOn(HttpClient,'get').and.returnValue(of(mockUserList));
+    httpClientSpy.get.and.returnValue(of(mockUserList));
+    let userList:User[];
+    service.getUserList().subscribe(list =>{
+      userList =list;
+      expect(userList[0].name).toEqual('Thanu');
+    });   
+    expect(httpClientSpy.get).toHaveBeenCalled();   
   });
 });
